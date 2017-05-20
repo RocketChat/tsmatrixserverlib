c386c27fb3f4ec4cf1fbcf953cee32b32c999023
madguy02 <manishkakoti688@gmail.com>
2017-05-21 01:32:44 +0530

    transaction.js

---
+++ nodematrixserverlib/transaction.js
@@ -0,0 +1,22 @@
+//A transaction is basically used to push data
+//As an instance lets say (in rocket.chat terms) we push data from one hosted rocket.chat
+//instance to another hosted rocket.chat instance.Then transactions are handy.
+//(As used by matrix.org) used to push data from one matrix server to another.
+
+ // array is created to store list of PDU
+
+function TransactionID(transactionID){
+  this.transactionID = transactionID;
+  } 
+function Origin(origin){
+	this.origin = origin;
+
+}
+
+function Destination(destination){
+	this.destination = destination;
+
+}
+
+var previousID = []; // array is created to store the list of previousID's
+var PDUs = [];
