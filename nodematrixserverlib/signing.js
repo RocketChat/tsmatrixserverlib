4553e5ccfac2cb7d7d4d85b7f179eac2b520de0c
madguy02 <manishkakoti688@gmail.com>
2017-05-27 11:36:32 +0530

    started with signing.js

---
+++ nodematrixserverlib/signing.js
@@ -0,0 +1,20 @@
+var ed = require("ed25519-supercop");
+var HashMap = require("hashmap");
+
+var KeyID;
+var message;
+
+function SignJson( signingName,KeyID,PrivateKey,message){
+	var signatures;
+	var map = new HashMap();
+	map.set(signatures,KeyID);
+	var error = new Error("there is an error oops!!");
+	if (error){
+		JSON.parse(message);
+	}
+
+	if (error!=nil){
+		return nil,error;
+	}
+}
+
