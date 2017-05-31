decc118eebef5d9cf77d98a66f1d3535fac7b806
madguy02 <manishkakoti688@gmail.com>
2017-05-31 13:06:27 +0530

    added important comments

--- nodematrixserverlib/json.js
+++ nodematrixserverlib/json.js
@@ -7,3 +7,6 @@ var obj = {"b":"two","a":1}; //this is a json string used by matrix-org for test
 
 JSON.stringify(obj, Object.keys(obj).sort());  // has to be a function called sort SOrtJson
 
+//the basic idea is to reencode json and then arrange them into lexicographical order.
+//gophers has a very copmplex way to do it,but JS is somewhat simple :)
+// i guess there is nno need to use interfaces in js,as gophers use it to handle loosely handled or non-strictly typed json.
