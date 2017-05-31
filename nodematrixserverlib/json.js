6b414d830fa281d7de6b337609b5189d13d41d28
madguy02 <manishkakoti688@gmail.com>
2017-05-31 17:01:31 +0530

    changes few things

--- nodematrixserverlib/json.js
+++ nodematrixserverlib/json.js
@@ -7,9 +7,14 @@ var input = {
 //     return a.b.localeCompare(b.a);
 // });
 // //console.log(a.b.localeCompare(b.a));
+function sortJSON() {
+    JSON.stringify(input, Object.keys(input).sort());
+
+} // has to be a function called sort SortJson
+
 function CanonicalJSON(input, err) {
-    for (var i = 0; i < input.length; i++) {
-        var sorted = SortJSON(input);
+    for (var i = 0; i < input.length; i++) { // i guess i have to use slices innstes creating forloops 
+        var sorted = sortJSON(input);
 
 
         if (err != null) {
@@ -21,10 +26,7 @@ function CanonicalJSON(input, err) {
     }
 }
 
-function SortJSON() {
-    JSON.stringify(input, Object.keys(input).sort());
 
-} // has to be a function called sort SortJson
 
 //the basic idea is to reencode json and then arrange them into lexicographical order.
 //gophers has a very copmplex way to do it,but JS is somewhat simple :)
