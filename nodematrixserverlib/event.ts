c0436f3c35f6cc62361fa569bfc338a60e54aebc
madguy02 <manishkakoti688@gmail.com>
2017-06-04 23:38:40 +0530

    added a new function to event.ts

--- nodematrixserverlib/event.ts
+++ nodematrixserverlib/event.ts
@@ -1,23 +1,27 @@
-interface StateKeyTuple{
-  EventType:string;
-  StateKey:string;
-}
+  interface StateKeyTuple{
+    EventType:string;
+    StateKey:string;
+  }
 
-interface EventReference {
-  EventID:string;
-  EventSHA256:Base64String;
-}
+  interface EventReference {
+    EventID:string;
+    //EventSHA256:Base64String;
+  }
 
-interface EventBuilder {
-  Sender:string;
-  RoomID:string;
-  Type:string;
-  StateKey: string;
-   PrevEvents: EventReference[];
-   AuthEvents: EventReference[];
-   Redacts: string;
-   Depth: number;
-   Content: string;
-   Unsigned: string;
+  interface EventBuilder {
+    Sender:string;
+    RoomID:string;
+    Type:string;
+    StateKey: string;
+     PrevEvents: EventReference[];
+     AuthEvents: EventReference[];
+     Redacts: string;
+     Depth: number;
+     Content: string;
+     Unsigned: string;
 
-}
+  }
+
+var SetContent: EventBuilder = function (Content: string) {
+      return JSON.stringify(Unsigned);
+  }
