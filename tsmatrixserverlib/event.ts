83ffd30c8f818ca4f5c5c5088c84f8120f9b74a0
madguy02 <manishkakoti688@gmail.com>
2017-06-06 11:54:59 +0530

    few things added in event.ts

--- tsmatrixserverlib/event.ts
+++ tsmatrixserverlib/event.ts
@@ -1,88 +1,112 @@
-  interface StateKeyTuple{
-    EventType:string;
-    StateKey:string;
-  }
+interface StateKeyTuple {
+  EventType: string;
+  StateKey: string;
+}
 
-  interface EventReference {
-    EventID:string;
-    //EventSHA256:Base64String;
-  }
+interface EventReference {
+  EventID: string;
+  //EventSHA256:Base64String;
+}
 
-  interface EventBuilder {
-    Sender:string;
-    RoomID:string;
-    Type:string;
-    StateKey: string;
-     PrevEvents: EventReference[];
-     AuthEvents: EventReference[];
-     Redacts: string;
-     Depth: number;
-     Content: string;
-     Unsigned: string;
+interface EventBuilder {
+  Sender: string;
+  RoomID: string;
+  Type: string;
+  StateKey: string;
+  PrevEvents: EventReference[];
+  AuthEvents: EventReference[];
+  Redacts: string;
+  Depth: number;
+  Content: string;
+  Unsigned: string;
 
-  }
+}
 
-var SetContent: EventBuilder = function (Content: string) {
-      return JSON.stringify(Content);
-  }
+var SetContent: EventBuilder = function(Content: string) {
+  return JSON.stringify(Content);
+}
 
-var SetUnsigned: EventBuilder = function (Unsigned: string) {
-    return JSON.stringify(Unsigned);
+var SetUnsigned: EventBuilder = function(Unsigned: string) {
+  return JSON.stringify(Unsigned);
 }
 interface eventFields {
-    RoomID:string;
-    EventID:string;
-    Sender:string;
-    Type:string;
-    StateKey:string;
-    Content:string;
-    PrevEvents:EventReference[];
-    AuthEvents: EventReference[];
-    Redacts:string;
-    Depth:number;
-    Unsigned:string;
-    OriginServerTS:Timestamp;
-    Origin:ServerName;
+  RoomID: string;
+  EventID: string;
+  Sender: string;
+  Type: string;
+  StateKey: string;
+  Content: string;
+  PrevEvents: EventReference[];
+  AuthEvents: EventReference[];
+  Redacts: string;
+  Depth: number;
+  Unsigned: string;
+  OriginServerTS: Timestamp;
+  Origin: ServerName;
 
 }
 
-var emptyEventReferenceList:EventReference[];
+var emptyEventReferenceList: EventReference[];
 
 // there are few things here which i do not know Build() in golang
 
-let eventJSON:string= [];
+let eventJSON: string = [];
 
-if (eventJSON === JSON.stringify(event)){
-    return;
+if (eventJSON === JSON.stringify(event)) {
+  return;
 }
-if (eventJSON===addContentHashesToEvent(eventJSON)){
-    return;
+if (eventJSON === addContentHashesToEvent(eventJSON)) {
+  return;
 }
 
-if (eventJSON===signEvent(origin:string),keyID,privateKey,eventJSON){
-   return;
+if (eventJSON === signEvent(origin:string), keyID, privateKey, eventJSON) {
+  return;
 }
-if (eventJSON ===CanonicalJSON(eventJSON)){
-   return;
+if (eventJSON === CanonicalJSON(eventJSON)) {
+  return;
 }
 result.eventJSON = eventJSON;
 
 var err;
 
-if (err === JSON.stringify(eventJSON,result.fields)){
-    return;
+if (err === JSON.stringify(eventJSON, result.fields)) {
+  return;
 }
 
-if (err === result.CheckFields()){
-   return;
+if (err === result.CheckFields()) {
+  return;
 }
 return;
 }
 
-function newEventFromUntrustedJSON(eventJSON[]:Event){
-    let map = new Map<string, string>();
-    map.set("event", "rawJSON");
-    if (err === JSON.stringify(eventJSON,event)){
-        return;
+function newEventFromUntrustedJSON(eventJSON[]:Event) {
+  let map = new Map<string, string>();
+  map.set("event", "rawJSON");
+  if (err === JSON.stringify(eventJSON, event)) {
+    return;
+  }
+  delete (event, "outlier");
+  delete (event, "destinations");
+  delete (event, "age_ts");
+
+  if (eventJSON === JSON.stringify(event)) {
+    return;
+  }
+  if (err === checkEventContentHash(eventJSON)) {
+    result.redacted = true;
+    if (eventJSON === redactEvent(eventJSON)) {
+      return;
     }
+  }
+  if (eventJSON === CanonicalJSON(eventJSON)) {
+    return;
+  }
+  result.eventJSON = eventJSON;
+  if (err === JSON.stringify(eventJSON, result.fields)) {
+    return;
+  }
+  if (err === CheckFields()) {
+    return;
+  }
+  return;
 }
