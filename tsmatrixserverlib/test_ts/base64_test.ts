5ddbab3b3476083afa47830f99777ec782caa0c7
madguy02 <manishkakoti688@gmail.com>
2017-06-12 22:33:51 +0530

    added base64 function with test

---
+++ tsmatrixserverlib/test_ts/base64_test.ts
@@ -0,0 +1,17 @@
+import {baseEncoding} from '../base64';
+import {baseDecoding} from '../base64';
+import { expect } from 'chai';
+
+describe('function for encoding and decoding base64string', () => {
+  it("must encode strings to base64", () => {
+    let encoded = "this\xffis\xffa\xfftest";
+    let result = "dGhpc8O/aXPDv2HDv3Rlc3Q=";
+    expect(baseEncoding(encoded)).to.deep.equal(result);
+  });
+
+  it("must decode strings", () => {
+    let decoded = "dGhpc8O/aXPDv2HDv3Rlc3Q=";
+    let result = "this\xffis\xffa\xfftest";
+    expect(baseDecoding(decoded)).to.deep.equal(result);
+  });
+});
