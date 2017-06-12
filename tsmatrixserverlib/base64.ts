9ab921d132a1095e47a072eec5f7ff67bffcd3df
madguy02 <manishkakoti688@gmail.com>
2017-06-12 22:36:03 +0530

    base64

---
+++ tsmatrixserverlib/base64.ts
@@ -0,0 +1,15 @@
+// A Base64String is a string of bytes that are base64 encoded when used in JSON.
+// The bytes encoded using base64 when parsed as JSON.
+// When the bytes are parsed from JSON they are decoded from base64.
+// parsing JSON decodes a JSON string and then decodes the resulting base64.
+// This takes a pointer receiver because it needs to write the result of decoding.
+declare const Buffer;
+export function baseEncoding(tobeencoded) {
+    let encoded = new Buffer(tobeencoded).toString("base64");
+    return encoded;
+}
+
+export function baseDecoding(tobedecoded) {
+    let decoded = new Buffer(tobedecoded, "base64").toString();
+    return decoded;
+}
