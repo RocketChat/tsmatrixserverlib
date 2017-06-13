da42b6e2b8183df16924d08929611d2a0a854f87
madguy02 <manishkakoti688@gmail.com>
2017-06-14 03:23:36 +0530

    init version of resolve.ts

--- tsmatrixserverlib/resolve.ts
+++ tsmatrixserverlib/resolve.ts
@@ -11,15 +11,21 @@ interface DNSResult{
   Host: HostResult;
   //Addrs: []string;
 }
-let serverName;
+let serverName:string;
+
 function LookupServer(serverName:DNSResult){
   let result;
+  let str1:any;
+  let val:any = str1.indexOf(serverName);
   //result.Hosts = HostResult{};
+
   let hosts = serverName.SRVRecords;
-  //if (serverName:string == -1){
+
+  if(val === -1)
+  {
     result.SRVCName  = dns.lookup("rocket.chat","tcp",serverName);
     result.SRVRecords = dns.lookup("rocket.chat","tcp",serverName);
-//  }
+  }
   let err;
   if (err!=null){
     dns.TIMEOUT();
