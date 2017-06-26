877281a22752549f05077961295f54010758b319
madguy02 <manishkakoti688@gmail.com>
2017-06-26 20:17:45 +0530

    added a simple test to examine the functions if callable locally

--- tsmatrixserverlib/json.ts
+++
@@ -1,7 +0,0 @@
-function readHexDigits(input:any){
-  let buffer = new ArrayBuffer(32);
-  let dataview = new DataView(buffer);
-  let hex = 0x30303030 - 0;
-  let result = hex.toString(32);
-
-}
---
+++ tsmatrixserverlib/test.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var timestamp_1 = require("./timestamp");
+var myValidator = timestamp_1.asTimeStamp("2009 02 13 23:31:30");
+console.log(myValidator);
