06dfa6c68a1d3e0b7e4b14047306a21d179ec4d4
madguy02 <manishkakoti688@gmail.com>
2017-07-06 22:36:22 +0530

    updated redactevent

--- tsmatrixserverlib/redactevent.ts
+++ tsmatrixserverlib/redactevent.ts
@@ -1,21 +1,54 @@
 function redactEvent(eventJSON:string){
   interface createContent{
-    creator:string;
+    creator:undefined;
+
   }
-  // function omitempty(createContent): {creator:string } {
-  //   let newomit = {creator: undefined};
+
 }
 interface joinRulesContent{
-    join_rule:string;
+    join_rule:undefined;
 }
 
 interface powerLevelContent{
-    users:string;
-    users_default:string;
-    events: string;
-    events_default: string;
-    state_default: string;
-    ban: string;
-    kick: string;
-    redact: string;
+    users:undefined;
+    users_default:undefined;
+    events: undefined;
+    events_default: undefined;
+    state_default: undefined;
+    ban: undefined;
+    kick: undefined;
+    redact: undefined;
+}
+
+interface memberContent{
+    membership: undefined;
+}
+
+interface aliasesContent{
+    aliases: undefined;
+}
+
+interface historyVisibilityContent{
+    history_visibility: undefined;
+}
+
+// not able to understand allContent in https://github.com/matrix-org/gomatrixserverlib/blob/master/redactevent.go#L104
+
+interface eventFields{
+    event_id: undefined;
+    sender: undefined;
+    room_id: undefined;
+    hashes: undefined;
+    signatures: undefined;
+    content: undefined;
+    type: undefined;
+    state_key: undefined;
+    depth: undefined;
+    prev_events: undefined;
+    prev_state: undefined;
+    auth_events: undefined;
+    origin: undefined;
+    origin_server_ts: undefined;
+    membership: undefined;
+
 }
