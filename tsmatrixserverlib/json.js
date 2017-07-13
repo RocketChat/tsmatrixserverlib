2b183bdf97ed7370904f1e1193d23981289d56b5
madguy02 <manishkakoti688@gmail.com>
2017-07-13 23:06:19 +0530

    json.ts

--- tsmatrixserverlib/json.js
+++
@@ -1,6 +0,0 @@
-function readHexDigits(input) {
-    var buffer = new ArrayBuffer(32);
-    var dataview = new DataView(buffer);
-    var hex = 0x30303030 - 0;
-    var result = hex.toString(32);
-}
---
+++ tsmatrixserverlib/json.ts
@@ -0,0 +1,17 @@
+function sortJSON(input) {
+  let value = JSON.stringify(input, Object.keys(input).sort());
+  return value;
+}
+
+function readhexdigits(input: any){
+  let hex = input;
+  hex = - 0x30303030;
+  hex &= 0x1F1F1F1F;
+  let mask =  hex & 0x10101010;
+  hex -= mask >> 1;
+  hex += mask >> 4;
+  hex |= hex >> 4;
+  hex &= 0xFF00FF;
+  hex |= hex >> 8;
+  return hex & 0xFFFF;
+}
