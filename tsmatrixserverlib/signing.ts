923aa965c66ca24f8cd0753751913c4740eeaa37
madguy02 <manishkakoti688@gmail.com>
2017-07-28 00:59:45 +0530

    updated signing.ts

--- tsmatrixserverlib/signing.ts
+++ tsmatrixserverlib/signing.ts
@@ -8,7 +8,7 @@ function signJson(jsonObject, signatureName, signingKey) {
     let signed = signingKey.sign(messageBytes);
     let signatureBase64 = btoa(signed.signature);
 
-    let keyId = "%s:%s" % (signingKey.alg, signingKey.version);
+    let keyId =  (signingKey.alg, signingKey.version);
 
     //signatures.setdefault(signatureName, {})[keyId] = signatureBase64;
     jsonObject["signatures"] = signatures;
@@ -17,5 +17,41 @@ function signJson(jsonObject, signatureName, signingKey) {
     }
 
     return jsonObject;
+
+}
+
+function ListKeyIDs(signingname:string,message:any[]){
+  //let keyID:string[];
+  //let Signatures= Map<string, keyID> = new Map<string, keyID>();
+}
+
+
+function VerifyJson(jsonObject, signatureName, verifyKey) {
+    try {
+        var signatures = jsonObject["signatures"];
+    } catch (Error) {
+        throw new Error("No signatures on this object");
+    }
+
+    let keyId = (verifyKey.alg, verifyKey.version);
+
+    try {
+        //var signatureB64 = signatures[signatureName][keyId];
+    } catch (Error) {
+        throw new Error("Missing signature");
+
+    }
+
+    try {
+         //var signature = btoa(signatureB64);
+    } catch (Error) {
+        throw new Error("Invalid signature base64");
+    }
+    var dict;
+    var jsonObjectCopy = dict(jsonObject);
+    delete jsonObjectCopy["signatures"];
+    delete jsonObjectCopy["unsigned"];
+    var message = JSON.stringify(JSON.parse(jsonObjectCopy));
+
 
 }
