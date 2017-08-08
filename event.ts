dfc001b746d318152f592d2b1c2eb003f6de9615
madguy02 <manishkakoti688@gmail.com>
2017-08-08 18:30:03 +0530

    updated event.ts

--- event.ts
+++ event.ts
@@ -1,3 +1,4 @@
+import * as TimeStamp from './timestamp';
 interface StateKeyTuple {
   EventType: string;
   StateKey: string;
@@ -16,4 +17,25 @@ interface EventBuilder {
   Redacts: string;
   Depth: number;
   Content: string;
+}
+let eb: EventBuilder;
+function SetContent() {
+eb.Content = 'content';
+return;
+}
+
+interface  Event {
+redacted: boolean;
+fields: EventFields;
+}
+
+interface EventFields {
+RoomID: string;
+EventID: string;
+Sender: string;
+Type: string;
+Content: string;
+Redacts: string;
+OriginServerTS: string;
+Origin: string;
 }
--- test.ts
+++ test.ts
@@ -1,8 +1,8 @@
 
-import { asTimeStamp } from "./timestamp";
-import {baseEncoding} from "./base64";
-import {baseDecoding} from "./base64";
-import {signJson} from "./signing";
-let myValidator = asTimeStamp("2009 02 13 23:31:30");
-let myValidator1 = baseEncoding("this\xffis\xffa\xfftest");
-let myValidator2 = baseDecoding("dGhpc8O/aXPDv2HDv3Rlc3Q=");
+import { asTimeStamp } from './timestamp';
+import {baseEncoding} from './base64';
+import {baseDecoding} from './base64';
+import {signJson} from './signing';
+let myValidator = asTimeStamp('2009 02 13 23:31:30');
+let myValidator1 = baseEncoding('this\xffis\xffa\xfftest');
+let myValidator2 = baseDecoding('dGhpc8O/aXPDv2HDv3Rlc3Q=');
