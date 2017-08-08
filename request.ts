54e6de8991995c4268c980f70db10f6f890f9712
madguy02 <manishkakoti688@gmail.com>
2017-08-08 12:02:48 +0530

    added function readHTTPRequest

--- request.ts
+++ request.ts
@@ -1,4 +1,5 @@
 import http = require('http');
+import fs = require('fs');
 interface Fields {
 Content: string;
 Destination: string;
@@ -31,11 +32,23 @@ function HTTPRequest() {
 let urlStr = sprintf('matrix://%s%s', FederationRequest.Destination, FederationRequest.RequestURI);
 let Content;
 if (FederationRequest.Content != null) {
-Content = JSON.parse(FederationRequest.Content);
+Content = fs.readFileSync(FederationRequest.Content);
   }
 let httpreq;
 httpreq = http.request(urlStr);
 if (httpreq.URL.RequestURI() !== FederationRequest.RequestURI) {
 Error('Did not encode properly');
   }
+if (FederationRequest.Content != null) {
+httpreq.setHeader('Content-Type', 'application/json');
+  }
+return httpreq;
+}
+
+function readHTTPRequest(req, res, Body: string) {
+let result: Fields;
+result.Method = req.Method;
+result.RequestURI = req.URL.RequestURI();
+let content = JSON.parse(req.Body);
+
 }
