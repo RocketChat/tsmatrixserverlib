c83e49c386f78eea1afc12b7f9558e74c1878c23
madguy02 <manishkakoti688@gmail.com>
2017-08-08 07:48:16 +0530

    updated request.go

--- request.ts
+++ request.ts
@@ -1,4 +1,15 @@
-interface Fields{
-  Content:"content";
-  Destination:"destination"
+interface Fields {
+Content: 'content';
+Destination: string;
+Method: string;
+Origin: 'origin';
+RequestURI: string;
+}
+
+function NewFederationRequest (method: string, destination: string, requestURI: string) {
+let FederationRequest: Fields;
+FederationRequest.Destination = destination;
+FederationRequest.Method = method.toUpperCase();
+FederationRequest.RequestURI = requestURI;
+return FederationRequest;
 }
