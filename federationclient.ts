4e41c572adafd19ddd587e64dedafe5c6fb58a11
madguy02 <manishkakoti688@gmail.com>
2017-08-13 08:26:12 +0530

    updated federationclient.ts

--- federationclient.ts
+++ federationclient.ts
@@ -1,22 +1,29 @@
-import http = require('http');
-import request = require('request');
+let https = require('https');
+let request = require('request');
+
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+let bodyString = JSON.stringify ({
+'message': 'hello'
+});
 let options = {
     host: 'localhost',
     port: 8448,
-    path: '/_matrix/client/#/',
-    method: 'GET',
+    path: '/_matrix/federation/v1/send_join/room/!rlukPqHNcfqfWVHndH',
+    method: 'PUT',
     headers: {
-        accept: 'application/json'
+'Authorization': 'X-Matrix origin=madguy02:my.domain.name',
+'sig': 'ABCDEF...',
+'key': 'ed25519:key1',
+'Content-Type': 'application/json',
+'Content-Length': bodyString.length
     }
 };
 
 console.log('Start');
-let x = http.request(options, function(res){
+let x = https.request(options, function(res){
     console.log('Connected');
     res.on('data', function(data){
         console.log(data);
+        console.log(String.fromCharCode.apply(null, new Uint16Array(data)));
     });
-});
-
-x.end();
+}).write(bodyString);
