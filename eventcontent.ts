43e913cedfad02df8ce41f5ea5ce5e99c46e6cda
madguy02 <manishkakoti688@gmail.com>
2017-08-13 21:10:38 +0530

    updated eventcontent

--- eventcontent.ts
+++ eventcontent.ts
@@ -1,79 +1,80 @@
-interface createContent{
-  senderDomain:string;
-  RoomID:string;
-  eventID:string;
-  Federate:"m.federate";
-  Creator:"creator";
+interface createContent {
+  senderDomain: string;
+  RoomID: string;
+  eventID: string;
+  Federate:'m.federate';
+  Creator:'creator';
   }
-  let senderDomain;
-  let Federate;
-  function newCreateContentFromAuthEvents(authEvents){
-  var createEvent;
-  if (createEvent == authEvents.Create()){
+let senderDomain;
+let Federate;
+function newCreateContentFromAuthEvents(authEvents) {
+let createEvent;
+if (createEvent === authEvents.Create()) {
       return;
   }
-  if (createEvent == null){
-    console.log("missing create event");
+if (createEvent === null) {
+    console.log('missing create event');
     return;
   }
   //one more if
 let roomID = createEvent.RoomID();
 let eventID = createEvent.EventID();
 //domainFromID call
-if (senderDomain = domainFromID(createEvent.Sender())){
+if (senderDomain = domainFromID(createEvent.Sender())) {
   return;
   }
 return;
 }
 
-export function domainAllowed(domain:string){
-  if (domain ==senderDomain ){
+export function domainAllowed(domain: string) {
+  if (domain === senderDomain ) {
     return null;
   }
-  if (Federate == null){
+  if (Federate === null) {
     return null;
   }
-  console.log("room is unfederable");
+  console.log('room is unfederable');
 
 }
 
-function domainFromID(id:string){
+function domainFromID(id: string) {
   //code to be written
 }
 
-interface memberContent{
-  Membership:string;
+interface memberContent {
+  Membership: string;
   ThirdPartyInvite: undefined;
 }
 
-export function newMemberContentFromAuthEvents(authEvents,userID:string){
+export function newMemberContentFromAuthEvents(authEvents, userID: string){
   let memberEvent;
   let leave;
-  if (memberEvent==authEvents.Member(userID)){
+if (memberEvent === authEvents.Member(userID)) {
     return;
   }
-  if(memberEvent==null){
-    let Membership=leave;
+
+if (memberEvent === null) {
+let Membership = leave;
   }
   return newMemberContentFromEvent(memberEvent);
 }
 
-function newMemberContentFromEvent(event){
+export function newMemberContentFromEvent(event) {
 
 }
 
-interface joinRuleContent{
+interface joinRuleContent {
   JoinRule: string;
 }
 
-export function newJoinRuleContentFromAuthEvents(authEvents,joinRuleContent){
+export function newJoinRuleContentFromAuthEvents(authEvents,joinRuleContent) {
 let joinRulesEvent;
 let invite;
 let JoinRule;
-if (joinRulesEvent == authEvents.JoinRules()){
+if (joinRulesEvent === authEvents.JoinRules()) {
     return;
   }
-  if (joinRulesEvent == null){
+if (joinRulesEvent === null) {
     JoinRule = invite;
     return;
   }
