2af468bc6c2c6ec62b629f6a73e541ad59952849
madguy02 <manishkakoti688@gmail.com>
2017-08-16 14:50:47 +0530

    updated redactevent(required)

--- redactevent.ts
+++ redactevent.ts
@@ -1,86 +1,19 @@
-
-export function redactEvent(eventJSON: string) {
-
-  interface createContent {
-    Creator: "creator";
-
+function redactEvent(eventJSON: string) {
+interface CreateContent {
+Creator: string | null | undefined;
+  }
+interface JoinRulesContent {
+JoinRule: string | null | undefined;
   }
 
-
-  interface joinRulesContent {
-    JoinRule: "join_rule";
-  }
-
-  interface powerLevelContent {
-    Users: "users";
-    UsersDefault: "users_default";
-    Events: "events";
-    EventsDefault: "events_default";
-    StateDefault: "state_default";
-    Ban: "ban";
-    Kick: "kick";
-    Redact: "redact";
-  }
-
-  interface memberContent {
-    Membership: "membership";
-  }
-
-  interface aliasesContent {
-    Aliases: "aliases";
-  }
-
-  interface historyVisibilityContent {
-    HistoryVisibility: "history_visibility";
-  }
-  type allContent = createContent | joinRulesContent | powerLevelContent | memberContent | aliasesContent | historyVisibilityContent;
-
-
-  interface eventFields {
-    EventID: "event_id";
-    Sender: "sender";
-    RoomID: "room_id";
-    Hashes: "hashes";
-    Signatures: "signatures";
-    Content: "content";
-    Type: string;
-    StateKey: "state_key";
-    Depth: "depth";
-    PrevEvents: "prev_events";
-    AuthEvents: "auth_events";
-    Origin: "origin";
-    OriginServerTS: "origin_server_ts";
-    Membership: "membership";
-
-  }
-
-  let event: eventFields;
-  let newContent: allContent;
-  let joinrule: joinRulesContent;
-  let powerlevel: powerLevelContent;
-  let historyvisibility: historyVisibilityContent;
-  let aliascontent: aliasesContent;
-
-  function eventfunction(method: allContent) {
-
-    switch (event.Type) {
-      case "MRoomCreate":
-        return event.Content;
-
-      case "MRoomMember":
-        return event.Membership;
-
-      case "MRoomjoinRules":
-        return joinrule.JoinRule;
-
-      case "MRoomPowerLevels":
-        return powerlevel;
-
-      case "MRoomHistoryVisibility":
-        return historyvisibility;
-
-      case "MRoomAliases":
-        return aliascontent;
-    }
+interface  PowerLevelContent {
+Users: string | null | undefined;
+UsersDefault: string | null | undefined;
+Events: string |null | undefined;
+EventsDefault: string | null | undefined;
+StateDefault: string | null | undefined;
+Ban: string | null | undefined;
+Kick: string | null | undefined;
+Redact: string | null | undefined;
   }
 }
