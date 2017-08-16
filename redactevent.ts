45d4161711c35385a7af4b758cf874f77f5e9d2b
madguy02 <manishkakoti688@gmail.com>
2017-08-16 15:43:17 +0530

    updated redactevent.ts

--- redactevent.ts
+++ redactevent.ts
@@ -29,4 +29,24 @@ HistoryVisibility: string | null | undefined;
   }
 type AllContent = CreateContent | JoinRulesContent | PowerLevelContent | MemberContent | AliasesContent | HistoryVisibilityContent;
 
+interface EventFields {
+EventID: string | null | undefined;
+Sender: string | null | undefined;
+RoomID: string | null | undefined;
+Hashes: string | null | undefined;
+Signatures: string | null | undefined;
+Content: AllContent;
+Type: string;
+StateKey: string | null | undefined;
+Depth: string | null | undefined;
+PrevEvents: string | null | undefined;
+PrevState: string | null | undefined;
+AuthEvents: string | null | undefined;
+Origin: string | null | undefined;
+OriginServerTS: string | null | undefined;
+  }
+let event: EventFields;
+JSON.stringify(event);
+let NewContent: AllContent;
+
 }
