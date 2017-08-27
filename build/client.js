d6fee92b22641417a6a73af968dfab038f5a41c2
madguy02 <manishkakoti688@gmail.com>
2017-08-27 13:24:06 +0530

    changed file name

--- build/client.js
+++ build/client.js
@@ -3,10 +3,10 @@ exports.__esModule = true;
 var http = require("http");
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 var req = http.request({
-    host: '127.0.0.1',
-    port: 3000,
-    path: '/general',
-    method: 'PUT'
+    host: 'localhost',
+    port: 8448,
+    path: '/_matrix/federation/v1/query/directory?room_alias=%23test%3Alocalhost%3A8448HTTP/1.1',
+    method: 'GET'
 }, function (res) {
     var body = [];
     res.on('data', function (data) {
