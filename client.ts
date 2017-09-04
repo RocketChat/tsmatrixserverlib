d3b572b0f428409c8c3de3b033a06973a80f9b2f
madguy02 <manishkakoti688@gmail.com>
2017-09-04 13:27:09 +0530

    added tls.connect to client.ts

--- client.ts
+++ client.ts
@@ -1,4 +1,6 @@
 import http = require('http');
+import tls = require('tls');
+import fs = require('fs');
 import HttpTransport = require('http-transport');
 let transport = new HttpTransport();
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
@@ -24,4 +26,17 @@ req.end();
 
 req.on('error', function(err){
     console.log(err);
+});
+let options = {
+pfx: fs.readFileSync('key.pfx')
+};
+let socket = tls.connect(8008, options, () => {
+  console.log('client connected',
+              socket.authorized ? 'authorized' : 'unauthorized');
+  process.stdin.pipe(socket);
+  process.stdin.resume();
+});
+socket.setEncoding('utf8');
+socket.on('data', (data) => {
+  console.log(data);
 });
