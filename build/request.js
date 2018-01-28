6633bc796d456bd1caafa12344312c4121be212a
madguy02 <manishkakoti688@gmail.com>
2018-01-28 22:02:54 +0530

    some init changes were done on resolve.ts

--- build/request.js
+++ build/request.js
@@ -1,56 +1,82 @@
 "use strict";
 exports.__esModule = true;
-var http = require("http");
-var fs = require("fs");
-var sprintf = require("sprintf");
-var FederationRequest;
-function NewFederationRequest(method, destination, requestURI) {
-    FederationRequest.Destination = destination;
-    FederationRequest.Method = method.toUpperCase();
-    FederationRequest.RequestURI = requestURI;
-    return FederationRequest;
+var https = require("https");
+var options = {
+    method: 'GET',
+    path: '/_matrix/federation/v1/send/',
+    host: 'federation.rocket.chat',
+    port: '8089'
+};
+console.log('Federating to matrix......');
+var x = https.request(options, function (res) {
+    console.log('Connected');
+    res.on('data', function (data) {
+        console.log(data);
+        // console.log(String.fromCharCode.apply(null, new Uint16Array()));
+    });
+});
+x.end();
+/* export class Fields {
+Content: string;
+Destination: string;
+Method: string;
+Origin: string;
+RequestURI: string;
+constructor(destination: string, method: string, requestURI: string) {
+this.Destination = destination;
+this.Method = method;
+this.RequestURI = requestURI;
+  }
+
+// }
+// let FederationRequest: Fields;
+NewFederationRequests() {
+// FederationRequest.Destination = destination;
+// FederationRequest.Method = method.toUpperCase();
+// FederationRequest.RequestURI = requestURI;
+return this.Destination + '' + this.Method + this.RequestURI;
 }
-exports.NewFederationRequest = NewFederationRequest;
-function Method() {
-    return FederationRequest.Method;
+
+/*Method() {
+return this.Method;
+} */
+/*Content() {
+return this.Content;
+}*/
+/*export function RequestURI() {
+return this.RequestURI;
+}*/
+/* HTTPRequest() {
+let urlStr = sprintf('matrix://%s%s', this.Destination, this.RequestURI);
+let Content;
+if (this.Content != null) {
+Content = fs.readFileSync(this.Content);
+  }
+let httpreq;
+httpreq = https.request(urlStr);
+if (httpreq.URL.RequestURI() !== this.RequestURI) {
+Error('Did not encode properly');
+  }
+if (this.Content != null) {
+httpreq.setHeader('Content-Type', 'application/json');
+  }
+return httpreq;
 }
-exports.Method = Method;
-function Content() {
-    return FederationRequest.Content;
-}
-exports.Content = Content;
-function RequestURI() {
-    return FederationRequest.RequestURI;
-}
-exports.RequestURI = RequestURI;
-function HTTPRequest() {
-    var urlStr = sprintf('matrix://%s%s', FederationRequest.Destination, FederationRequest.RequestURI);
-    var Content;
-    if (FederationRequest.Content != null) {
-        Content = fs.readFileSync(FederationRequest.Content);
+
+readHTTPRequest(req, res, Body: string) {
+let result: Fields;
+result.Method = req.Method;
+result.RequestURI = req.URL.RequestURI();
+let content = JSON.parse(req.Body);
+if (content.length !== 0) {
+if (req.getHeader('Content-Type') !== 'application/json') {
+Error('The request must be application/json');
     }
-    var httpreq;
-    httpreq = http.request(urlStr);
-    if (httpreq.URL.RequestURI() !== FederationRequest.RequestURI) {
-        Error('Did not encode properly');
-    }
-    if (FederationRequest.Content != null) {
-        httpreq.setHeader('Content-Type', 'application/json');
-    }
-    return httpreq;
-}
-exports.HTTPRequest = HTTPRequest;
-function readHTTPRequest(req, res, Body) {
-    var result;
-    result.Method = req.Method;
-    result.RequestURI = req.URL.RequestURI();
-    var content = JSON.parse(req.Body);
-    if (content.length !== 0) {
-        if (req.getHeader('Content-Type') !== 'application/json') {
-            Error('The request must be application/json');
-        }
-    }
-    result.Content = content;
-    return result;
-}
-exports.readHTTPRequest = readHTTPRequest;
+  }
+result.Content = content;
+return result;
+  }
+} */
+// let Field = new Fields('localhost:8448', 'GET', '/_matrix/federation/v1/query/directory?room_alias=%23test%3Alocalhost%3A8448');
+// console.log(Field);
+// console.log(https.request());
--- resolve.ts
+++ resolve.ts
@@ -1,25 +1,14 @@
 import dns = require('dns');
-/*dns.resolve('federation.rocket.chat', 'A', function(err, rec) {
-if (err) {
-console.log(err);
- }
-console.log(rec);
-}); */
-
-// here i have met only 1 condition with resolving ip addresses
-// resolving using ports is not being implemented
-dns.resolve4('federation.rocket.chat', function (err, addresses) {
-if (err) throw err;
-console.log('addresses: ' + JSON.stringify(addresses));
-addresses.forEach(function (a) {
-    dns.reverse(a, function (err, domains) {
-      if (err) {
-        console.log('reverse for ' + a + ' failed: ' +
-          err.message);
-      } else {
-        console.log('reverse for ' + a + ': ' +
-          JSON.stringify(domains));
-      }
+dns.resolve4('matrix.org', function (err, addresses) {
+        if (err)
+            throw err;
+        console.log('addresses: ' + JSON.stringify(addresses));
+        addresses.forEach(function (a) {
+            dns.reverse(a, function (err, matrix) {
+                if (err) {
+                    throw err;
+                }
+                console.log('reverse for ' + a + ': ' + JSON.stringify(matrix));
+            });
+        });
     });
-  });
-});
