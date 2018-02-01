fa63d5254883cb47c4d5e455c5d90eea2c7480d7
madguy02 <manishkakoti688@gmail.com>
2018-02-02 00:32:22 +0530

    few more UT case added

--- test/TS/keys_test.ts
+++ test/TS/keys_test.ts
@@ -1,6 +1,5 @@
-import {GenerateSigningKey} from '../../keys';
+import {GenerateSigningKey, EncodeSigningKeyBase64, DecodeSigningKeyBase64, GetVerifyKey} from '../../keys';
 import { expect } from 'chai';
-// import { assert } from 'assert';
 import {} from 'mocha';
 
 describe('Test for generation of keys', () => {
@@ -9,6 +8,17 @@ describe('Test for generation of keys', () => {
     let my_key = GenerateSigningKey(my_version);
     expect(my_key).to.have.property('publicKey');
     expect(my_key).to.have.property('secretKey');
-    // expect(my_key).to.equal('my_version');
     });
   });
+
+describe('Encode and verify signing keys', () => {
+    it('encode to base 64 and verify it', () => {
+      let my_version = 'my_version';
+      let key = GenerateSigningKey(my_version);
+      let key_base64 = EncodeSigningKeyBase64(key);
+      let decode_key = DecodeSigningKeyBase64(key_base64);
+      let get_verify_key = GetVerifyKey(key);
+      expect(decode_key).to.have.property('publicKey');
+      expect(decode_key).to.have.property('secretKey');
+      });
+    });
