0cec750d719310b7063d0dcc12a379fe3fa72395
madguy02 <manishkakoti688@gmail.com>
2018-02-01 23:45:38 +0530

    updated keys with tests

--- keys.ts
+++ keys.ts
@@ -1,20 +1,23 @@
 import nacl = require('tweetnacl');
+let NACL_ED25519 = 'ed25519';
 
-function GenerateSigningKey(version) {
-let key = nacl.sign.keyPair();
+export function GenerateSigningKey(version) {
+let key = nacl.sign.keyPair(); // nacl.sign.keyPair();
 // key.version = version;
+// key.alg = NACL_ED25519;
 return key;
 }
 
 function GetVerifyKey(signing_key) {
 let verify_key = signing_key.verify_key;
-verify_key = signing_key.version;
+verify_key.version = signing_key.version;
+
 return verify_key;
 }
 
 function DecodeSigningKeyBase64(key_base64) {
 let key_bytes = new Buffer(key_base64, 'base64').toString('ascii');
-let key = nacl.sign.keyPair();
+let key = GenerateSigningKey(key_bytes);
 return key;
 }
 
@@ -45,43 +48,43 @@ return key;
 }
 }
 
-function ReadSigningKeys(stream) {
-let keys = [];
-let line;
-for (line in stream) {
-key_base64 = line.split();
-let key = new Buffer(key_base64, 'base64').toString('ascii');
-keys.push(key);
-}
-}
+// function ReadSigningKeys(stream) {
+// let keys = [];
+// let line;
+// for (line in stream) {
+// key_base64 = line.split();
+// let key = new Buffer(key_base64, 'base64').toString('ascii');
+// keys.push(key);
+// }
+// }
 
-function WriteSigningKeys(stream, keys) {
-let key;
-for (key in keys) {
-key_base64 = new Buffer(key, 'base64').toString('ascii');
-stream.write(key_base64);
-}
-}
-let my_version = 'my_version';
-let key = GenerateSigningKey(my_version);
-console.log(key);
-let newkey = key.toString();
+// function WriteSigningKeys(stream, keys) {
+// let key;
+// for (key in keys) {
+// key_base64 = new Buffer(key, 'base64').toString('ascii');
+// stream.write(key_base64);
+// }
+// }
+// let my_version = 'my_version';
+// let key = GenerateSigningKey(my_version);
+// console.log(key);
+// let newkey = key.toString();
 
-let key_base64 = EncodeSigningKeyBase64(newkey);
-let verify_key = GetVerifyKey(key);
-console.log(key_base64);
-console.log(verify_key);
+// let key_base64 = EncodeSigningKeyBase64(newkey);
+// let verify_key = GetVerifyKey(key);
+// console.log(key_base64);
+// console.log(verify_key);
 
-let newkeybase64 = key_base64.toString();
-let decoder = DecodeSigningKeyBase64(newkeybase64);
-console.log(decoder);
+// let newkeybase64 = key_base64.toString();
+// let decoder = DecodeSigningKeyBase64(newkeybase64);
+// console.log(decoder);
 
-// var signing_key_seed = DecodeSigningKeyBase64("YJDBA9Xnr2sVqXD9Vj7XVUnmFZcZrlw8Md7kMW+3XA1");
-let seed = 'YJDBA9Xnr2sVqXD9Vj7XVUnmFZcZrlw8Md7kMW+3XA1';
-let b = new Buffer(seed, 'base64');
-let s = b.toString();
-console.log(s);
-let setup = nacl.sign.keyPair.fromSeed(b).toString();
+// // var signing_key_seed = DecodeSigningKeyBase64("YJDBA9Xnr2sVqXD9Vj7XVUnmFZcZrlw8Md7kMW+3XA1");
+// let seed = 'YJDBA9Xnr2sVqXD9Vj7XVUnmFZcZrlw8Md7kMW+3XA1';
+// let b = new Buffer(seed, 'base64');
+// let s = b.toString();
+// console.log(s);
+// let setup = nacl.sign.keyPair.fromSeed(b).toString();
 
-console.log(setup);
+// console.log(setup);
 // console.log(signing_key_seed);
