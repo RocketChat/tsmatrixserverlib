e942d917a2cf1b3233456bc3475ae5a88c63ec47
madguy02 <manishkakoti688@gmail.com>
2018-02-10 20:33:54 +0530

    updated event.ts

--- event.ts
+++ event.ts
@@ -1,6 +1,9 @@
 import {asTimeStamp} from './timestamp';
 import {utcTime} from './timestamp';
 import {redactEvent} from './redactevent';
+import {addContentHashesToEvent} from  './eventcrypto';
+import {signEvent} from './eventcrypto';
+
 interface StateKeyTuple {
   EventType: string;
   StateKey: string;
@@ -8,7 +11,7 @@ interface StateKeyTuple {
 
 interface EventReference {
   EventID: string;
-  EventSHA256: string;
+  EventSHA256;
 }
 
 export interface EventBuilder {
@@ -16,19 +19,27 @@ export interface EventBuilder {
   RoomID: string;
   Type: string;
   StateKey: string;
+  PrevEvents;
+  AuthEvents;
   Redacts: string;
   Depth: number;
-  Content: string;
+  Content;
+  Unsigned;
 }
 let eb: EventBuilder;
-function SetContent() {
-eb.Content = 'content';
+
+function SetContent(content) {
+eb.Content = content;
 return;
+}
+
+function SetUnsigned(unsigned) {
+eb.Unsigned = unsigned;
 }
 
 export interface  Event {
 redacted: boolean;
-eventJSON: string;
+eventJSON;
 fields: EventFields;
 }
 
@@ -37,75 +48,140 @@ RoomID: string;
 EventID: string;
 Sender: string;
 Type: string;
-Content: string;
+StateKey;
+Content;
+PrevEvents;
+AuthEvents;
 Redacts: string;
-OriginServerTS: number;
-Origin: string;
+Depth: number;
+Unsigned;
+OriginServerTS;
+Origin;
+PrevState;
 }
-let event: EventFields;
+let emptyEventReferenceList = [];
+// let event: EventFields;
 
-export function Build(eventID: string, now: string, origin: string, ) {
+export function Build(eventID: string, now, origin, keyID, privatekey ) {
+let result: Event;
+// type event = EventBuilder | Origin | OriginServerTS | eventID;
+let event: EventFields;
+if (event.PrevEvents === null) {
+event.PrevEvents = emptyEventReferenceList;
+}
+if (event.AuthEvents === null) {
+event.AuthEvents = emptyEventReferenceList;
+}
 event.OriginServerTS = asTimeStamp(now);
 event.Origin = origin;
 event.EventID = eventID;
-}
-let result: Event;
-function NewEventFromTrustedJSON(eventJSON: string, redacted) {
 
+if (event.StateKey !== null) {
+event.PrevState = emptyEventReferenceList;
+}
+let eventJSON;
+if (eventJSON === event) {
+return;
+}
+if (eventJSON === addContentHashesToEvent(eventJSON)) {
+return;
+}
+if (eventJSON === signEvent(origin, keyID, privatekey, eventJSON)) {
+return;
+}
+// if (eventJSON === eventJSON) {
+
+// } // TBD
+result.eventJSON = eventJSON;
+return;
+}
+// let result: Event;
+function NewEventFromTrustedJSON(eventJSON, redacted) {
+let result: Event;
 result.redacted = redacted;
 result.eventJSON = eventJSON;
 return;
 }
-let e: Event;
+
 function Redacted() {
+let e: Event;
 return e.redacted;
 }
 
 function JSON() {
+let e: Event;
 return e.eventJSON;
 }
 
 function Redact() {
+let e: Event;
 if (e.redacted) {
 return e;
 }
-
-// let eventJSON = redactEvent(e.eventJSON); // redactEvent has to be added to redactEvent.ts
+let eventJSON = redactEvent(e.eventJSON);
+let result =  { redacted: true, eventJSON: eventJSON};
 result.redacted = true;
 // result.redacted = eventJSON;
 return result;
 }
 
 export function Origin() {
-return event.Origin;
+let fields: EventFields;
+return fields.Origin;
 }
 
 export function EventID() {
-return event.EventID;
+let fields: EventFields;
+return fields.EventID;
 }
 
 export function Sender() {
-return event.Sender;
+let fields: EventFields;
+return fields.Sender;
 }
 
 export function Type() {
-return event.Type;
+let fields: EventFields;
+return fields.Type;
 }
 
 export function OriginServerTS() {
-return event.OriginServerTS;
+let fields: EventFields;
+return fields.OriginServerTS;
 }
 
 export function Content() {
-return event.Content;
+let fields: EventFields;
+return fields.Content;
+}
+
+export function AuthEvents() {
+let fields: EventFields;
+return fields.AuthEvents;
+}
+
+export function AuthEventIDs() {
+let fields: EventFields;
+let result;
+for (let i of fields.AuthEvents) {
+result[i] = fields.AuthEvents[i].EventID;
+}
+return result;
 }
 
 export function Redacts() {
-return event.Redacts;
+let fields: EventFields;
+return fields.Redacts;
 }
 
 export function RoomID() {
-return event.RoomID;
+let fields: EventFields;
+return fields.RoomID;
+}
+
+export function Depth() {
+let fields: EventFields;
+return fields.Depth;
 }
 
 export function SplitID(address) {
