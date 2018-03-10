c617d3e99044f1b796e573be21f2d9ae489d5324
madguy02 <manishkakoti688@gmail.com>
2018-03-10 15:14:45 +0530

    added utility for tsmatrixserverlib

---
+++ utility/ServerDiscovery.ts
@@ -0,0 +1,17 @@
+// import srv = require('dns-srv');
+// import net = require('net');
+
+// let sock = new net.Stream();
+// let connector = srv.connect(sock // This socket will become connected if everything goes well
+//          , ['_matrix._tcp'] // The SRV record to query
+//          , 'matrix.org'           // The domain whose DNS SRV we are interested in
+//          , 8008                // Default fallback port to connect to in case SRV lookup failed
+// );
+
+// connector.on('error', function() { console.error('meh...'); }).
+//           on('connect', function() { console.log('yeah baby!!'); });
+
+import dns = require('dns');
+dns.resolveSrv('matrix.org', function(err, result){
+console.log(JSON.stringify(result));
+});
---
+++ utility/fallback.ts
@@ -0,0 +1,17 @@
+import dns = require('dns');
+function ResolveAddr(address) {
+dns.resolve6(address, function(err, result){
+// if (err) throw err;
+if (result === undefined) {
+dns.resolve4(address, function(err, result){
+console.log(JSON.stringify(result));
+});
+}
+else {
+console.log(JSON.stringify(result));
+}
+});
+
+}
+
+ResolveAddr('matrix.org');
