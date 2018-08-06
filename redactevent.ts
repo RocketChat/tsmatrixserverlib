c5105fcfee574abfbd1e3ece13ea8682a9c9decd
madguy02 <manishkakoti688@gmail.com>
2018-08-07 02:05:05 +0530

    updated redactEvent

--- redactevent.ts
+++ redactevent.ts
@@ -1,12 +1,13 @@
 import {MRoomCreate, MRoomMember, MRoomJoinRules, MRoomPowerLevels, MRoomHistoryVisibility, MRoomAliases} from './eventAuth';
-export interface CreateContent {
+export function redactEvent(eventJson) {
+interface CreateContent {
 Creator;
   }
-export interface JoinRulesContent {
+interface JoinRulesContent {
 JoinRule;
   }
 
-export interface  PowerLevelContent {
+interface  PowerLevelContent {
 Users;
 UsersDefault;
 Events;
@@ -17,18 +18,18 @@ Kick;
 Redact;
 }
 
-export interface MemberContent {
+interface MemberContent {
 Membership;
   }
-export interface AliasesContent {
+interface AliasesContent {
 Aliases;
   }
 
-export interface  HistoryVisibilityContent {
+interface  HistoryVisibilityContent {
 HistoryVisibility;
   }
 // type AllContent = CreateContent | JoinRulesContent | PowerLevelContent | MemberContent | AliasesContent | HistoryVisibilityContent;
-export interface AllContent {
+interface AllContent {
 CreateContent;
 JoinRulesContent;
 PowerLevelContent;
@@ -38,7 +39,7 @@ HistoryVisibilityContent;
 
 }
 
-export interface EventFields {
+interface EventFields {
 EventID;
 Sender;
 RoomID;
@@ -55,8 +56,41 @@ Origin;
 OriginServerTS;
   }
 
-export let event: EventFields;
-export function redactEvent(eventJSON: string) {
+let event: EventFields = <any>{};
+JSON.parse(eventJson);
+
+let newContent: AllContent = <any>{};
+
+switch(event.Type) {
+
+case MRoomCreate: {
+  newContent.CreateContent = event.Content.CreateContent;
+}
+case MRoomMember: {
+  newContent.MemberContent = event.Content.MemberContent;
+}
+
+case MRoomJoinRules: {
+  newContent.JoinRulesContent = event.Content.JoinRulesContent; 
+}
+
+case MRoomPowerLevels: {
+  newContent.PowerLevelContent = event.Content.PowerLevelContent;
+}
+
+case MRoomHistoryVisibility: {
+  newContent.HistoryVisibilityContent = event.Content.HistoryVisibilityContent;
+}
+
+case MRoomAliases: {
+  newContent.AliasesContent = event.Content.AliasesContent;
+}
+}
+event.Content = newContent;
+return JSON.stringify(event);
+}
+// export let event: EventFields;
+// export function redactEvent(eventJSON: string) {
 // interface CreateContent {
 // Creator;
 //   }
@@ -113,41 +147,40 @@ export function redactEvent(eventJSON: string) {
 // OriginServerTS;
 //   }
 // let event: EventFields;
-JSON.stringify(event);
-let NewContent: AllContent;
+// 
 // let membercontent: MemberContent;
 // let joinrulescontent: JoinRulesContent;
 // let powerlevelcontent: PowerLevelContent;
 // let historyvisibilitycontent: HistoryVisibilityContent;
 // let aliasescontent: AliasesContent;
-switch (event.Type) {
-case MRoomCreate: {
-NewContent.CreateContent === event.Content.CreateContent;
-return event.Content;
-}
-case MRoomMember: {
- NewContent.MemberContent = event.Content.MemberContent;
+// switch (event.Type) {
+// case MRoomCreate: {
+// NewContent.CreateContent === event.Content.CreateContent;
+// return event.Content;
+// }
+// case MRoomMember: {
+//  NewContent.MemberContent = event.Content.MemberContent;
 // return membercontent;
-}
-case MRoomJoinRules: {
-NewContent.JoinRulesContent = event.Content.JoinRulesContent;
- // return joinrulescontent;
-}
-case MRoomPowerLevels: {
-NewContent.PowerLevelContent = event.Content.PowerLevelContent;
-// return powerlevelcontent;
-}
-case MRoomHistoryVisibility: {
-NewContent.HistoryVisibilityContent = event.Content.HistoryVisibilityContent;
-// return historyvisibilitycontent;
-}
-case MRoomAliases: {
-NewContent.AliasesContent = event.Content.AliasesContent;
-// return aliasescontent;
-}
-  }
+// }
+// case MRoomJoinRules: {
+// NewContent.JoinRulesContent = event.Content.JoinRulesContent;
+//  // return joinrulescontent;
+// }
+// case MRoomPowerLevels: {
+// NewContent.PowerLevelContent = event.Content.PowerLevelContent;
+// // return powerlevelcontent;
+// }
+// case MRoomHistoryVisibility: {
+// NewContent.HistoryVisibilityContent = event.Content.HistoryVisibilityContent;
+// // return historyvisibilitycontent;
+// }
+// case MRoomAliases: {
+// NewContent.AliasesContent = event.Content.AliasesContent;
+// // return aliasescontent;
+// }
+//   }
 
-event.Content = NewContent;
-return event;
+// event.Content = NewContent;
+// return event;
 
-}
+// }
